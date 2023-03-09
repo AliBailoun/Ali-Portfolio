3a83d378c1ed601c274eef034fcdc44594a446f8
Ali Bailoun <alibailoun1982@gmail.com>
2023-03-09 04:11:47 -0500

    testing output again

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
 export default function Card(props) {
+  const cardStyle = {
+    width: '25rem',
+    height: '40rem'
+  };
+
   return (
-    <div className='container'>
-      <h1>Projects:</h1>
-      <ul className='list-group'>
-        {props.modules.map((module) => (
-          <li className='list-group-item' key={props.modules.id}>
-            {`${props.modules.title} ${props.modules.description} ${props.modules.depLink} ${props.modules.repLink} ${props.modules.image}`}
-          </li>
-        ))}
-      </ul>
+    <div className='d-flex flex-wrap '>
+      {props.modules.map(item => (
+        <div className="card m-3 bg-light p-2 shadow-sm" key={item.id} style={cardStyle}>
+          <div className="bg-image">
+            <img
+              src={item.image}
+              className="img-fluid h-100 w-100 rounded shadow-sm "
+              alt="Sample"
+            />
+          </div>
+          <div className="card-body d-flex flex-column justify-content-between">
+            <h6 className="card-title h4 text-center shadow-sm rounded bg-dark color-w">{item.title}</h6>
+            <p className="card-text text-center ">{item.description}</p>
+            <div className="d-flex justify-content-between ">
+              <a href={item.repoLink} className="btn btn-light shadow">
+                View Github
+              </a>
+              <a href={item.depLink} className="btn btn-dark shadow">
+                View Deployed
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
