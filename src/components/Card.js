6aaf268e90a1d2d756d296718d2be46d59bccf3b
Ali Bailoun <alibailoun1982@gmail.com>
2023-03-09 03:51:34 -0500

    fixed app.js and added props to card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,26 +3,20 @@ import Card from 'react-bootstrap/Card';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-function Card() {
+export default function Card(props) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{props.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {props.description}
         </Card.Text>
       </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
       <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href={props.depLink}>Deployed Link</Card.Link>
+        <Card.Link href={props.repoLink}>Repo Link</Card.Link>
       </Card.Body>
     </Card>
   );
 }
-
-export default Card;
