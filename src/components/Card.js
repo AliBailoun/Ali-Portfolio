05bc819a62d2c6072e2652b61e5855ddef3f9f54
Ali Bailoun <alibailoun1982@gmail.com>
2023-03-09 04:00:55 -0500

    testing output

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,16 @@
-import Card from 'react-bootstrap/Card';
-
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React from 'react';
 
 export default function Card(props) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          {props.description}
-        </Card.Text>
-      </Card.Body>
-      <Card.Body>
-        <Card.Link href={props.depLink}>Deployed Link</Card.Link>
-        <Card.Link href={props.repoLink}>Repo Link</Card.Link>
-      </Card.Body>
-    </Card>
+    <div className='container'>
+      <h1>Projects:</h1>
+      <ul className='list-group'>
+        {props.modules.map((module) => (
+          <li className='list-group-item' key={props.modules.id}>
+            {`${props.modules.title} ${props.modules.description} ${props.modules.depLink} ${props.modules.repLink} ${props.modules.image}`}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
